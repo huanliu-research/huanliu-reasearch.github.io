090656db4cd48e257f9467c2cd7e65ad7bfc8e61
huanliu-research <huanliu@caltech.edu>
2025-09-27 04:59:54 +0000

    Deploying to gh-pages from @ huanliu-research/liuhuan.github.io@11f16fe0325cb503ea9a37f40789a80c231cedef 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "huanliu-research/huanliu-research.github.io",
+    "data-repo": "huanliu-research/liuhuan.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
